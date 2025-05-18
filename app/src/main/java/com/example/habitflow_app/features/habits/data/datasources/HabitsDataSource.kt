003789e523bd28c1af2e8f7ca1d51ac52157a385
Pablo Pineda <pinedapablo6718@gmail.com>
2025-05-17 21:27:59 -0500

    feat: add updateHabit in habits datasource

--- app/src/main/java/com/example/habitflow_app/features/habits/data/datasources/HabitsDataSource.kt
+++ app/src/main/java/com/example/habitflow_app/features/habits/data/datasources/HabitsDataSource.kt
@@ -3,6 +3,7 @@ package com.example.habitflow_app.features.habits.data.datasources
 import com.example.habitflow_app.core.network.DirectusApiService
 import com.example.habitflow_app.domain.models.Habit
 import com.example.habitflow_app.features.habits.data.dto.HabitRequest
+import com.example.habitflow_app.features.habits.data.dto.HabitUpdateRequest
 import javax.inject.Inject
 
 class HabitsDataSource @Inject constructor(
@@ -24,6 +25,15 @@ class HabitsDataSource @Inject constructor(
             return response.body() ?: throw Exception("Empty response body")
         } else {
             throw Exception("Failed to create habit: ${response.errorBody()?.string()}")
+        }
+    }
+
+    suspend fun updateHabit(habitId: String, request: HabitUpdateRequest): Habit {
+        val response = directusApiService.updateHabit(habitId, request)
+        if (response.isSuccessful) {
+            return response.body() ?: throw Exception("Empty response")
+        } else {
+            throw Exception("Failed to update habit: ${response.errorBody()?.string()}")
         }
     }
 }
