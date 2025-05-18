ec1269879a374384a0800f166e9d4b2d04f5a0dd
Pablo Pineda <pinedapablo6718@gmail.com>
2025-05-17 19:18:38 -0500

    feat: update habits repository with createHabit method

--- app/src/main/java/com/example/habitflow_app/domain/repositories/HabitsRepository.kt
+++ app/src/main/java/com/example/habitflow_app/domain/repositories/HabitsRepository.kt
@@ -1,7 +1,9 @@
 package com.example.habitflow_app.domain.repositories
 
 import com.example.habitflow_app.domain.models.Habit
+import com.example.habitflow_app.features.habits.data.dto.HabitRequest
 
 interface HabitsRepository {
     suspend fun getHabits(userId: String): List<Habit>
+    suspend fun createHabit(request: HabitRequest): Habit
 }
--- app/src/main/java/com/example/habitflow_app/features/habits/data/repositories/HabitsRepositoryImpl.kt
+++ app/src/main/java/com/example/habitflow_app/features/habits/data/repositories/HabitsRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.example.habitflow_app.features.habits.data.repositories
 import com.example.habitflow_app.domain.models.Habit
 import com.example.habitflow_app.domain.repositories.HabitsRepository
 import com.example.habitflow_app.features.habits.data.datasources.HabitsDataSource
+import com.example.habitflow_app.features.habits.data.dto.HabitRequest
 import javax.inject.Inject
 
 class HabitsRepositoryImpl @Inject constructor(
@@ -10,5 +11,9 @@ class HabitsRepositoryImpl @Inject constructor(
 ): HabitsRepository {
     override suspend fun getHabits(userId: String): List<Habit> {
         return habitsDataSource.getHabits(userId)
+    }
+
+    override suspend fun createHabit(request: HabitRequest): Habit {
+        return habitsDataSource.createHabit(request)
     }
 }
