47d1eec0a2d14a0b4c79d08c0a5709cab4946446
EstebanGZam <estebangaviria82@gmail.com>
2025-05-18 12:22:51 -0500

    refactor: name of the main habits screen changed

--- app/src/main/java/com/example/habitflow_app/features/habits/ui/screens/HabitsScreen.kt
+++ app/src/main/java/com/example/habitflow_app/features/habits/ui/screens/HabitsScreen.kt
@@ -6,9 +6,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview(showBackground = true)
 @Composable
-fun HabitsMainScreen() {
+fun HabitsScreen() {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
