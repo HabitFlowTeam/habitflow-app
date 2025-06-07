d60acc9556d2a09d0107c97646bf8893f62ac4a2
MGMaikeru <13miguegonza2005@gmail.com>
2025-06-06 22:54:05 -0500

    fix: eliminate articles ranked visualization

--- sql/DML.sql
+++ sql/DML.sql
@@ -98,7 +98,7 @@ VALUES
     -- Permiso para leer la vista user_habit_tracking_view (sin filtro para admins)
     ('a21cfc5d-3f01-4e45-8e93-dd0d440af562', 'user_habit_tracking_view', 'read', '*', '{"_and":[{"user_id":{"_eq":"$CURRENT_USER"}}]}'),
     -- Permiso para leer la vista ranked_articles_view (sin filtro para admins)
-    ('a21cfc5d-3f01-4e45-8e93-dd0d440af562', 'ranked_articles_view', 'read', '*', '{"_and":[{"user_id":{"_eq":"$CURRENT_USER"}}]}');
+    ('a21cfc5d-3f01-4e45-8e93-dd0d440af562', 'ranked_articles_view', 'read', '*', '{}');
     -- Permiso para leer la vista user_habit_categories_view (sin filtro para admins)
     ('a21cfc5d-3f01-4e45-8e93-dd0d440af562', 'user_habit_categories_view', 'read', '*', '{"_and":[{"user_id":{"_eq":"$CURRENT_USER"}}]}');
 
@@ -144,7 +144,7 @@ VALUES
     -- Permiso para leer la vista user_habit_tracking_view con filtro para el usuario actual
     ('5e8b7092-6ee2-47df-b24a-b3c9f733a9c4', 'user_habit_tracking_view', 'read', '*', '{"_and":[{"user_id":{"_eq":"$CURRENT_USER"}}]}'),
     -- Permiso para leer la vista ranked_articles_view con filtro para el usuario actual
-    ('5e8b7092-6ee2-47df-b24a-b3c9f733a9c4', 'ranked_articles_view', 'read', '*', '{"_and":[{"user_id":{"_eq":"$CURRENT_USER"}}]}');
+    ('5e8b7092-6ee2-47df-b24a-b3c9f733a9c4', 'ranked_articles_view', 'read', '*', '{}');
     -- Permiso para leer la vista user_habit_categories_view con filtro para el usuario actual
     ('5e8b7092-6ee2-47df-b24a-b3c9f733a9c4', 'user_habit_categories_view', 'read', '*', '{"_and":[{"user_id":{"_eq":"$CURRENT_USER"}}]}');
 
